a9fe46f6911a0792c58031854b0bbab44eea32bf
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-03-28 14:38:34 -0400

    duster tests

--- src/main/java/com/zipcodewilmington/froilansfarm/Vehicles/CropDuster.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Vehicles/CropDuster.java
@@ -1,4 +1,13 @@
 package com.zipcodewilmington.froilansfarm.Vehicles;
 
 public class CropDuster {
+    public void fly() {
+    }
+
+    public boolean isFlying() {
+        return true;
+    }
+
+    public void fertilize() {
+    }
 }
--- src/test/java/com/zipcodewilmington/froilansfarm/CropdusterTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/CropdusterTest.java
@@ -1,4 +1,37 @@
 package com.zipcodewilmington.froilansfarm;
 
+import com.zipcodewilmington.froilansfarm.Vehicles.CropDuster;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CropdusterTest {
+    @Nested
+    class CropDusterTest {
+
+        private CropDuster cropDuster;
+
+        @BeforeEach
+        void setUp() {
+            cropDuster = new CropDuster();
+        }
+
+        @Test
+        void testFly() {
+            cropDuster.fly();
+            assertTrue(cropDuster.isFlying(), "CropDuster should be flying");
+        }
+
+        @Test
+        void testFertilize() {
+            try {
+                cropDuster.fertilize();
+            } catch (Exception e) {
+                fail("Fertilize method threw an exception: " + e.getMessage());
+            }
+        }
+
+    }
 }
