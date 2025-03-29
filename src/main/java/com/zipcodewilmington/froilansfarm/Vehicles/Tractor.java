5a70c3ce467927f330ea343eb2006775dab7c677
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-03-29 09:46:20 -0400

    cropduster tests done

--- src/main/java/com/zipcodewilmington/froilansfarm/Vehicles/Tractor.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Vehicles/Tractor.java
@@ -1,4 +1,25 @@
 package com.zipcodewilmington.froilansfarm.Vehicles;
 
 public class Tractor {
+    public void operate() {
+    }
+
+    public boolean isOperating() {
+        return false;
+    }
+
+    public void harvest() {
+    }
+
+    public boolean isHarvesting() {
+        return false;
+    }
+
+    public Object ride() {
+        return null;
+    }
+
+    public String makeNoise() {
+        return "";
+    }
 }
--- src/test/java/com/zipcodewilmington/froilansfarm/TractorTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/TractorTest.java
@@ -1,4 +1,39 @@
 package com.zipcodewilmington.froilansfarm;
 
-public class TractorTest {
+import com.zipcodewilmington.froilansfarm.Vehicles.Tractor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TractorTest {
+
+    private Tractor tractor;
+
+    @BeforeEach
+    void setUp() {
+        tractor = new Tractor();
+    }
+
+    @Test
+    void testOperate() {
+        tractor.operate();
+        assertTrue(tractor.isOperating(), "Tractor should be operating after calling operate()");
+    }
+
+    @Test
+    void testHarvest() {
+        tractor.harvest();
+        assertTrue(tractor.isHarvesting(), "Tractor should be harvesting after calling harvest()");
+    }
+
+    @Test
+    void testRide() {
+        try {
+            tractor.ride();
+        } catch (Exception e) {
+            fail("Ride method threw an exception: " + e.getMessage());
+            fail("Ride method threw an exception: " + e.getMessage());
+        }
+    }
 }
