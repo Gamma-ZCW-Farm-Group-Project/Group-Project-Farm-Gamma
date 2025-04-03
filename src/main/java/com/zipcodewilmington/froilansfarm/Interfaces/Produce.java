0714545af69d039b1241293faee1c2642e681fe8
ConnorC04 <connorjcasey@iCloud.com>
2025-04-03 13:56:24 -0400

    updates produce's yield method

--- src/main/java/com/zipcodewilmington/froilansfarm/Interfaces/Produce.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Interfaces/Produce.java
@@ -1,5 +1,9 @@
 package com.zipcodewilmington.froilansfarm.Interfaces;
 
+import com.zipcodewilmington.froilansfarm.Edibles.Edible;
+
+import java.util.ArrayList;
+
 public interface Produce {
-    public boolean yield();
+    public void yield(ArrayList<Edible> e);
 }
