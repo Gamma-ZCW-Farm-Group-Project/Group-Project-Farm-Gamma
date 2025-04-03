63398cc0ec1a65844eb7e630a9cb4278fc4aaa25
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-04-03 13:53:09 -0400

    coffee ot corn you decide

--- src/main/java/com/zipcodewilmington/froilansfarm/Field/Crop.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Field/Crop.java
@@ -3,6 +3,24 @@ package com.zipcodewilmington.froilansfarm.Field;
 import com.zipcodewilmington.froilansfarm.Interfaces.Produce;
 
 public class Crop implements Produce {
+    private boolean hasBeenFertalized= false;
+    private boolean hasBeenHarvested= false;
+
+    public void Fertalized(){
+        hasBeenFertalized=true;
+    }
+    public void Harvested(){
+        hasBeenHarvested=true;
+    }
+
+    public boolean hasBeenFertalized() {
+        return hasBeenFertalized;
+    }
+
+    public boolean hasBeenHarvested() {
+        return hasBeenHarvested;
+    }
+
     @Override
     public boolean yield() {
 
