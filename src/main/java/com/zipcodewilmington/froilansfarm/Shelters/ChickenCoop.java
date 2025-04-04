7fd525a4284706ecbd5f34937944df6d78c0a1ef
GabrielC67 <gabecruzpro@gmail.com>
2025-04-04 09:02:18 -0400

    Updated chickenCoop

--- src/main/java/com/zipcodewilmington/froilansfarm/Shelters/ChickenCoop.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Shelters/ChickenCoop.java
@@ -5,18 +5,21 @@ import com.zipcodewilmington.froilansfarm.Animals.Chicken;
 import java.util.ArrayList;
 
 public class ChickenCoop extends Shelter{
-    public static ArrayList<Chicken> numOfChickens = new ArrayList<Chicken>(15);
+    public static ArrayList<Chicken> numOfChickens = new ArrayList<>();
 
     public void Chicken(){
-        this.numOfChickens = new ArrayList<>();
+        numOfChickens = new ArrayList<>();
     }
 
-    public ArrayList<Chicken> getNumOfChickens(){
-        return numOfChickens;
-    }
+//    public ArrayList<Chicken> getNumOfChickens(){
+//        return numOfChickens;
+//    }
 
     public void addChicken(Chicken chicken){
-        numOfChickens.add(chicken);
+        for (int i = 0; i < 15; i++) {
+            numOfChickens.add(chicken);
+        }
     }
+
 
 }
